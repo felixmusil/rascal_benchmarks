3badfe619063466d7bdce50430a9b0c6ea72de59
Félix <felix.musil@epfl.ch>
2020-06-15 17:54:54 +0200

    add prune operation

--- scripts/driver.py
+++ scripts/driver.py
@@ -6,6 +6,7 @@ import numpy as np
 import argparse
 from subprocess import Popen, PIPE, run
 from pathlib import Path
+import signac
 
 ROOT = os.path.dirname(__file__)
 back2root = ['&&','cd',ROOT]
@@ -18,8 +19,11 @@ if __name__ == '__main__':
                         help='init all signac project subfolders')
     parser.add_argument('--run', action='store_true',
                         help='run all signac project subfolders')
+    parser.add_argument('--prune', action='store_true',
+                        help="remove state points that don't have a document")
     parser.add_argument('-np','--parallel', type=int, default=-1,
                         help='set the number of process to use when running a project')
+
     args = parser.parse_args()
 
     if args.parallel == -1:
@@ -30,6 +34,14 @@ if __name__ == '__main__':
     if args.rm:
         print('Remove files from workspaces')
         run('rm -rf ./*/workspace/*',shell=True)
+
+    if args.prune:
+        for path in Path('./').rglob('init.py'):
+            print('    ',os.path.dirname(path))
+            project = signac.get_project(os.path.dirname(os.path.abspath(path)))
+            for job in project:
+                if len(job.document) < 1:
+                    job.remove()
 
     if args.init:
         print('Initialize all projects')
--- scripts/spherical_expansion/init.py
+++ scripts/spherical_expansion/init.py
@@ -18,9 +18,14 @@ fixed_params = [
 ]
 del_params = fixed_params
 fixed_params = {k:REPRESENTATION_PARAMS[0]['representation'][k] for k in fixed_params}
+fixed_accuracy = 1e-10
+
 
 for REPRESENTATION_PARAM in REPRESENTATION_PARAMS:
     if np.all([REPRESENTATION_PARAM['representation'][k] == v for k,v in fixed_params.items()]):
+        if 'accuracy' in REPRESENTATION_PARAM['representation']['optimization_args']:
+            if REPRESENTATION_PARAM['representation']['optimization_args']['accuracy'] != fixed_accuracy:
+                continue
         rep_args = deepcopy(REPRESENTATION_PARAM)
         rep_args['representation'] = {
             k:v for k,v in rep_args['representation'].items() if k not in del_params}
