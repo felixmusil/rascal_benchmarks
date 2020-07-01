22b8bcb541d9b82f4d18c8a50b77a6be3d552b57
felixmusil <felix.musil@epfl.ch>
2020-07-01 20:39:57 +0200

    make sure model is run on 1 cpu

--- scripts/model/project.py
+++ scripts/model/project.py
@@ -1,3 +1,6 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+
 # project.py
 from flow import FlowProject
 from subprocess import Popen, PIPE, run
@@ -23,6 +26,8 @@ from rascal.utils.random_filter import RandomFilter
 from rascal.utils.io import dump_obj,load_obj
 from ase.build import make_supercell
 from tqdm import tqdm
+
+
 
 group = {
     'sparse_point_fn' : 'sparse_point.json',
@@ -98,8 +103,8 @@ def compute_feature_selection(job):
     managers = soap.transform(frames)
     compressor = RandomFilter(soap, **sp['feature_subselection'])
     feature_subselection = compressor.select_and_filter(managers)
-    if sp['feature_subselection']['Nselect'] is None:
-        feature_subselection['coefficient_subselection'] = None
+    # if sp['feature_subselection']['Nselect'] is None:
+    #     feature_subselection['coefficient_subselection'] = None
     tojson(job.fn(group['feature_fn']), feature_subselection)
 
 @FlowProject.operation
