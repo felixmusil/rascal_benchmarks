901fb75aa04ad6ad29953e9b68df1fa06a720ac3
musil <felix.musil@epfl.ch>
2020-08-31 15:29:21 +0200

    train model

---
+++ scripts/train_model/init.py
@@ -0,0 +1,109 @@
+# init.py
+import signac
+import sys
+from copy import deepcopy
+import numpy as np
+from os.path import join,dirname
+from itertools import product
+sys.path.insert(0, join(dirname(__file__), '../'))
+from path import STRUCTURE_PATH, RASCAL_BUILD_PATH, BUILD_PATH
+
+project = signac.init_project('train_model')
+# Define parameter space
+
+names = ['silicon_bulk']
+seed = 10
+fns = {
+    'silicon_bulk': 'silicon_bulk.json',
+    'methane_liquid': 'methane_liquid.json',
+    'molecular_crystals': 'molecular_crystals_100.json',
+    'qm9': 'qm9.json',
+    'methane_sulfonic': 'methane_sulfonic_150.json',
+}
+
+misc_entries = {
+    'silicon_bulk' : {
+        'start_structure': 1,
+         "n_structures" : 2474,
+        # "n_structures" : 200,
+    },
+}
+
+
+global_species = {
+    'qm9' : [1,6,7,8,9],
+    'molecular_crystals' : [1, 6, 7, 8],
+    'silicon_bulk' : [14],
+    'methane_liquid' : [1, 6],
+    'methane_sulfonic' : [1,6,8,16],
+}
+
+n_sparse = [100, 200, 500, 1000, 2000, 5000, 7000, 9000]
+# n_sparse = [200]
+sparse_point_subselections = {
+    'qm9' : [dict(Nselect={1:int(v/5),6:int(v/5),7:int(v/5),8:int(v/5),9:int(v/5)}, act_on='sample per species', seed=seed) for v in n_sparse],
+    'molecular_crystals' : [dict(Nselect={1:int(v/4),6:int(v/4),7:int(v/4),8:int(v/4)}, act_on='sample per species', seed=seed) for v in n_sparse],
+    'silicon_bulk' : [dict(Nselect={14:v}, act_on='sample per species', seed=seed) for v in n_sparse],
+    'methane_liquid' : [dict(Nselect={1:int(v/2),6:int(v/2)}, act_on='sample per species', seed=seed) for v in n_sparse],
+    'methane_sulfonic' : [dict(Nselect={1:int(v/4),6:int(v/4),8:int(v/4),16:int(v/4)}, act_on='sample per species', seed=seed) for v in n_sparse],
+}
+
+models = {
+    'silicon_bulk' : [
+        {
+         'representation' :
+            dict(
+            interaction_cutoff=5., cutoff_smooth_width=1.,
+            max_radial=10, max_angular=12, gaussian_sigma_type="Constant",
+            soap_type="PowerSpectrum",
+            normalize=True,
+            expansion_by_species_method='structure wise',
+            global_species=global_species['silicon_bulk'],
+            compute_gradients=False,
+            cutoff_function_parameters=dict(),
+            cutoff_function_type="ShiftedCosine",
+            gaussian_sigma_constant=0.5,
+            coefficient_subselection=None,
+            radial_basis="GTO",
+            optimization_args={
+                  "type": "Spline", "accuracy": 1e-08, "range": [0, 5]
+                },
+            ),
+         'kernel':
+           dict(
+               name='GAP', zeta=4, target_type='Structure', kernel_type='Sparse'
+           ),
+        },
+    ]
+}
+
+f_feature = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7]
+# f_feature = [0.02]
+
+feature_subselections = {}
+for name,dd in models.items():
+    aa = dd[0]['representation']
+    # number of PowerSpectrum features
+    n_feat = int(aa['max_radial']**2 * (aa['max_angular']+1) * len(global_species[name])*(len(global_species[name])+1) /2)
+    feature_subselections[name] = [dict(Nselect=int(v*n_feat), act_on='feature', seed=seed) for v in f_feature]
+
+self_contributions = {
+    'silicon_bulk' : {14: -158.54496821},
+    'qm9': {1: 0, 6: 0, 7: 0, 8: 0, 9: 0},
+    'molecular_crystals': {1: -1.2066278536666357, 6: -18.42141665763611, 7: -28.21055160856482, 8: -41.63852285569995},
+    'methane_liquid': {1: 0, 6: 0},
+    'methane_sulfonic': {1: -0.6645519125911715, 6: -5.654232251386078, 8: -15.852522852103935, 16: -9.17258361289801}
+}
+
+for name in names:
+    for model, sparse_point_subselection, feature_subselection in product(models[name], sparse_point_subselections[name], feature_subselections[name]):
+        rep_args = deepcopy(model)
+        rep_args['name'] = name
+        rep_args['filename'] = join(STRUCTURE_PATH,fns[name])
+        rep_args['self_contributions'] = self_contributions[name]
+        rep_args['train_with_grad'] = True
+        rep_args['sparse_point_subselection'] = sparse_point_subselection
+        rep_args['feature_subselection'] = feature_subselection
+        rep_args.update(misc_entries[name])
+        job = project.open_job(rep_args)
+        job.init()
---
+++ scripts/train_model/project.py
@@ -0,0 +1,183 @@
+import os
+OMP_NUM_THREADS = 4
+os.environ["OMP_NUM_THREADS"] = "{}".format(OMP_NUM_THREADS)
+
+# project.py
+from flow import FlowProject
+from subprocess import Popen, PIPE, run
+from os.path import join,dirname
+from copy import deepcopy
+from ase.io import read
+import sys, os
+import numpy as np
+from time import sleep, ctime
+
+sys.path.insert(0, join(dirname(__file__), '../'))
+from path import STRUCTURE_PATH, RASCAL_BUILD_PATH, BUILD_PATH
+from utils.io import tojson, fromjson, fromfile, _decode
+from utils.model import KnmPool, train_gap_model
+from utils import Timer
+sys.path.insert(0, RASCAL_BUILD_PATH)
+from rascal.representations import SphericalInvariants, SphericalExpansion
+from rascal.representations.spherical_invariants import get_power_spectrum_index_mapping
+from rascal.representations import SphericalInvariants
+from rascal.models import Kernel, SparsePoints
+from rascal.neighbourlist import AtomsList
+from rascal.utils import from_dict, CURFilter, FPSFilter, fps, to_dict
+from rascal.utils.random_filter import RandomFilter
+from rascal.utils.io import dump_obj,load_obj
+from ase.build import make_supercell
+from tqdm import tqdm
+
+
+
+group = {
+    'sparse_point_fn' : 'sparse_point.json',
+    'feature_fn' : 'feature.json',
+    'knm_fn' : 'knm.npy',
+    'kernel_fn' : 'kernel.json',
+    'model_fn' : 'model.json',
+}
+
+en_field = {
+    'qm9': 'dHf_peratom',
+    'molecular_crystals': 'ENERGY',
+    'silicon_bulk': 'dft_energy',
+    'methane_liquid': 'energy',
+    'methane_sulfonic': 'energy',
+}
+
+force_field = {
+    'silicon_bulk': 'dft_force',
+}
+
+
+
+@FlowProject.label
+def feature_sel_computed(job):
+    return job.isfile(group['feature_fn'])
+@FlowProject.label
+def sparse_point_computed(job):
+    return job.isfile(group['sparse_point_fn'])
+@FlowProject.label
+def knm_computed(job):
+    return job.isfile(group['knm_fn'])
+@FlowProject.label
+def model_computed(job):
+    return job.isfile(group['model_fn'])
+
+
+@FlowProject.operation
+@FlowProject.post(feature_sel_computed)
+def compute_feature_selection(job):
+    sp = _decode(job.statepoint())
+    st,lg = job.sp.start_structure, job.sp.n_structures
+    frames = fromfile(job.sp.filename)[st:st+lg]
+    soap = SphericalInvariants(**sp['representation'])
+    managers = soap.transform(frames)
+    compressor = CURFilter(soap, **sp['feature_subselection'])
+    feature_subselection = compressor.select_and_filter(managers)
+
+    tojson(job.fn(group['feature_fn']), feature_subselection)
+
+@FlowProject.operation
+@FlowProject.pre.after(compute_feature_selection)
+@FlowProject.post(sparse_point_computed)
+def compute_sparse_point(job):
+    sp = _decode(job.statepoint())
+    st,lg = job.sp.start_structure, job.sp.n_structures
+    frames = fromfile(job.sp.filename)[st:st+lg]
+    soap = SphericalInvariants(**sp['representation'])
+    managers = soap.transform(frames)
+    compressor = CURFilter(soap, **sp['sparse_point_subselection'])
+    compressor.select(managers)
+
+    feature_subselection = fromjson(job.fn(group['feature_fn']))
+    sp['representation']['coefficient_subselection'] = feature_subselection['coefficient_subselection']
+    soap = SphericalInvariants(**sp['representation'])
+    managers = soap.transform(frames)
+    compressor._representation = soap
+    X_pseudo = compressor.filter(managers)
+    dump_obj(job.fn(group['sparse_point_fn']), X_pseudo)
+
+
+@FlowProject.operation
+@FlowProject.pre.after(compute_sparse_point)
+@FlowProject.post(knm_computed)
+def compute_knm(job):
+    sp = _decode(job.statepoint())
+    st,lg = job.sp.start_structure, job.sp.n_structures
+    frames = fromfile(job.sp.filename)[st:st+lg]
+
+    X_pseudo = load_obj(job.fn(group['sparse_point_fn']))
+    sp_fn = job.fn(group['sparse_point_fn'])
+
+    hypers = X_pseudo.representation._get_init_params()
+    hypers['compute_gradients'] = job.sp.train_with_grad
+    if job.sp.train_with_grad:
+        pool = KnmPool(ncpu=OMP_NUM_THREADS, energy_tag=en_field[job.sp.name], forces_tag=force_field[job.sp.name])
+    else:
+        pool = KnmPool(ncpu=OMP_NUM_THREADS, energy_tag=en_field[job.sp.name])
+
+    zeta = job.sp['kernel']['zeta']
+    res = pool.run(frames, zeta=zeta, sparse_points_fn=sp_fn, self_contributions=sp['self_contributions'])
+
+    np.save(job.fn(group['knm_fn']), res)
+
+def extract_ref(frames,info_key='energy',array_key='forces'):
+    y,f = [], []
+    for frame in frames:
+        y.append(frame.info[info_key])
+        if array_key is None:
+            pass
+        elif array_key == 'zeros':
+            f.append(np.zeros(frame.get_positions().shape))
+        else:
+            f.append(frame.get_array(array_key))
+    y= np.array(y)
+    try:
+        f = np.concatenate(f)
+    except:
+        pass
+    return y,f
+
+
+@FlowProject.operation
+@FlowProject.pre.after(compute_knm)
+@FlowProject.post(model_computed)
+def train_model_(job):
+    sp = _decode(job.statepoint())
+    st,lg = job.sp.start_structure, job.sp.n_structures
+    frames = fromfile(job.sp.filename)[st:st+lg]
+
+    X_pseudo = load_obj(job.fn(group['sparse_point_fn']))
+    hypers = X_pseudo.representation._get_init_params()
+    hypers['compute_gradients'] = True
+    soap = SphericalInvariants(**hypers)
+    kernel = Kernel(soap, **job.sp['kernel'])
+
+    res = np.load(job.fn(group['knm_fn']), allow_pickle=True).tolist()
+
+    if job.sp.train_with_grad:
+        model = train_gap_model(kernel, X_pseudo, res['energy'],
+                                sp['self_contributions'], grads=res['grads'],
+                                lambdas=[1e-3, 1e-2], jitter=1e-6)
+    else:
+        model = train_gap_model(kernel, X_pseudo, res['energy'],
+                                sp['self_contributions'],
+                                lambdas=[1e-3], jitter=1e-6)
+
+    dump_obj(job.fn(group['model_fn']), model)
+
+
+@FlowProject.operation
+@FlowProject.pre.after(train_model_)
+@FlowProject.post(lambda job: 'done' in job.document)
+def store_benchmark_in_document(job):
+    job.document = {'done':True}
+
+
+if __name__ == '__main__':
+    print(ctime())
+    FlowProject().main()
+    print(ctime())
