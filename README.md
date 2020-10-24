48af0e9aca2f1db7f747d856eb97f64a4592d5b0
felixmusil <felix.musil@epfl.ch>
2020-10-24 19:42:33 +0200

    typo

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ python driver.py --init --run -np 4 2>&1 | tee status.txt
 
 rsync -avzh cosmo3:/home/musil/git/rascal_benchmarks/scripts ./
 
-rsync -avzh --progress --max-size='10M' helvetios:/scratch/musil/rascal_benchmarks/scripts/* ./
+rsync -avzh --progress --max-size='5M' helvetios:/scratch/musil/rascal_benchmarks/scripts/* ./
 
 python driver.py --run -np 15 2>&1 | tee status.txt
 
--- scripts/model/project.py
+++ scripts/model/project.py
@@ -265,7 +265,7 @@ def train_model(job):
 @FlowProject.pre.after(train_model)
 @FlowProject.post(benchmark_computed)
 def compute_benchmark(job):
-    from rascal.models.krr import compute_forces
+    from rascal.models.krr import compute_sparse_kernel_gradients
     sp = _decode(job.statepoint())
     st,lg = job.sp.start_structure, job.sp.n_structures
     frames = fromfile(job.sp.filename)[st:st+lg]
@@ -316,7 +316,7 @@ def compute_benchmark(job):
             sleep(0.1)
             with timers['pred forces']:
                 rep = soap._representation
-                forces = compute_forces(rep, model.kernel._kernel, managers.managers, model.X_train._sparse_points, model.weights.reshape((1, -1)))
+                forces = -compute_sparse_kernel_gradients(rep, model.kernel._kernel, managers.managers, model.X_train._sparse_points, model.weights.reshape((1, -1)))
             sleep(0.1)
             managers, KNM = [], []
             del managers, KNM
